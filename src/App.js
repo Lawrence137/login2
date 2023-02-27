7f967cd49dbda496beabf74c3a942f092d78872a
Lawrence137 <lawrencekimani18@gmail.com>
2023-02-27 12:22:26 +0300

    login form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState  } from 'react';
+import { Container, Row, Col, Form, FormGroup, Label, Input, Button } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+function Login() {
+  const [showSignUp, setShowSignUp] = useState(false);
+  const handleClick = () => {
+    setShowSignUp(!showSignUp);
+  }
+  
+  const handleLogin = () => {
+    alert("Login successful!");
+  }
+
+  const handleSignup = () => {
+    setShowSignUp(false);
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col xs="12" md={{ size: 6, offset: 3 }} className={showSignUp ? "d-none" : ""}>
+          <Form>
+            <h2 className="text-center">Sign In</h2>
+
+            <FormGroup>
+              <Label for="username"></Label>
+              <Col xs="12" md={{ size: 6, offset: 3 }}>
+              <Input type="text" placeholder="username" required />
+              </Col>
+            </FormGroup>
+
+            <FormGroup>
+              <Label for="password"></Label>
+              <Col xs="12" md={{ size: 6, offset: 3 }}>
+              <Input type="password" placeholder="password" id="password" required />
+              </Col>
+            </FormGroup>
+
+            <FormGroup className="text-center">
+              <Label check>
+                <Input type="checkbox" /> Remember me
+              </Label>
+            </FormGroup>
+
+            <div className="d-flex justify-content-center">
+              <Button color="primary" className="btn-block" onClick={handleLogin}>Sign In</Button>
+            </div>
+
+            <br></br>
+
+            <div className="text-center">
+              {/* <a href="#1">Forgot password?</a> */}
+              <p>Not a member yet? <a href="#Sign Up" onClick={handleClick}>Sign Up here</a></p>
+            </div>
+
+          </Form>
+        </Col>
+      </Row>
+
+      { showSignUp ? (
+        <Row>
+          <Col xs="12" md={{ size: 6, offset: 3 }}>
+            <Form>
+              <h3 className="text-center">Sign Up</h3>
+
+              <FormGroup>
+                <Label for="username"></Label>
+                <Col xs="12" md={{ size: 6, offset: 3 }}>
+                  <Input type="text" placeholder="User Name" id="username" required className='form-control' />
+                </Col>
+              </FormGroup>
+
+              <FormGroup>
+                <Label for="email"></Label>
+                <Col xs="12" md={{ size: 6, offset: 3 }}>
+                  <Input type="email" placeholder="Email" id="email" required className='form-control'/>
+                </Col>
+              </FormGroup>
+
+              <FormGroup>
+                <Label for="password"></Label>
+                <Col xs="12" md={{ size: 6, offset: 3 }}>
+                  <Input type="password" placeholder="Password" id="password" required className='form-control' />
+                </Col>
+              </FormGroup>
+
+              <Col md={{ size: 6, offset: 5 }}>
+                <Button color="primary" className="btn-block" onClick={handleSignup}>Sign Up</Button>
+              </Col>
+            </Form>
+          </Col>
+        </Row>
+      )
+   : null }
+
+    </Container>
   );
 }
 
-export default App;
+export default Login;
